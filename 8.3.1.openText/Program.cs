0812fb44b4f1df877ad7485fc5a8ab261265fc65
Yulia Miller <miller82.98@mail.ru>
2023-02-11 21:25:40 -0800

    Добавьте файлы проекта.

---
+++ 8.3.1.openText/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+
+class FileWriter
+{
+    public static void Main()
+    {
+        string filePath = @"/Users/mille/SkillFactory/module_7/7.1.10_task/7.1.10_task/Program.cs";
+
+        using (StreamReader sr = File.OpenText(filePath))
+        {
+            string str = "";
+            while((str = sr.ReadLine()) != null)
+                Console.WriteLine(str);
+        }
+       
+
+    }
+}
